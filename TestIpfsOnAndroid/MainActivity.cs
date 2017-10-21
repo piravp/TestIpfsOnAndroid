3b0b148f8a1fb379827dcc3e367f5ae8d0d2aa2f
Piraveen <pira.rajan@gmail.com>
2017-10-21 03:35:58 +0200

    Initial commit

---
+++ TestIpfsOnAndroid/MainActivity.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace TestIpfsOnAndroid
+{
+    [Activity(Label = "TestIpfsOnAndroid", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        TextView view;
+
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            view = FindViewById<TextView>(Resource.Id.txtView);
+            view.Text = "Hello";
+        }
+    }
+}
+
